034a18bc7cadaa0e5b38820c00537224653f7e46
Kartik Visweswaran <kartikv2@gmail.com>
2014-11-06 22:59:11 +0530

    Initial release

---
+++ Config.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2014
+ * @package yii2-krajee-base
+ * @version 1.0.0
+ */
+
+namespace kartik\base;
+
+use Yii;
+use yii\base\InvalidConfigException;
+
+/**
+ * Global helper class for Krajee extensions
+ *
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ * @since 1.0
+ */
+class Config
+{
+    const VENDOR_NAME = "kartik-v/";
+    const NAMESPACE_PREFIX = "\\kartik\\";
+    const DEFAULT_REASON = "for your selected functionality";
+
+    /**
+     * Validate a single extension dependency
+     * @param string name the extension class name (without vendor namespace prefix)
+     * @param string repo the extension package repository name (without vendor name prefix)
+     * @param string reason a user friendly message for dependency validation failure
+     * @throws InvalidConfigException if extension fails dependency validation
+     */
+    public static function checkDependency($name, $repo, $reason = self::DEFAULT_REASON) {
+        if (empty($name)) {
+            return;
+        }
+        $class = self::NAMESPACE_PREFIX . $name;
+        if (!class_exists($class)) {
+            throw new InvalidConfigException("The class '{$class}' was not found and is required {$reason}.\n\n".
+                "Please ensure you have installed the '{$repo}' extension. To install you can run from console in your application root:\n\n" .
+                "php composer.phar require " . self::VENDOR_NAME . $repo ": \"@dev\" \n");
+        }
+    }
+
+    /**
+     * Validate multiple extension dependencies
+     * @param array extensions the configuration of extensions with each array 
+     * item setup as required in `checkDependency` method. The following keys
+     * can be setup:
+     * - name: string, the extension class name (without vendor namespace prefix)
+     * - repo: string,  the extension package repository name (without vendor name prefix)
+     * - reason: string,  a user friendly message for dependency validation failure
+     * @throws InvalidConfigException if extension fails dependency validation
+     */
+    public static function checkDependencies($extensions = []) {
+        foreach ($extensions as $extension) {
+            $name = '';
+            $repo = '';
+            $reason = self::DEFAULT_REASON;
+            extract($extension);
+            self::checkDependency($name, $repo, $reason);
+        }
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,4 +1,43 @@
-yii2-krajee
-===========
+yii2-krajee-base
+================
 
-Foundation classes for Yii2 used by Krajee extensions
+This is a base library and components for all [Yii2 extensions by Krajee](http://demos.krajee.com). One can use this base library for creating one's own extensions if needed.
+
+> NOTE: This extension depends on the [yiisoft/yii2-bootstrap](https://github.com/yiisoft/yii2/tree/master/extensions/bootstrap) extension. Check the 
+[composer.json](https://github.com/kartik-v/yii2-krajee-base/blob/master/composer.json) for this extension's requirements and dependencies. 
+
+## Installation
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+> Note: Read this [web tip /wiki](http://webtips.krajee.com/setting-composer-minimum-stability-application/) on setting the `minimum-stability` settings for your application's composer.json.
+
+Either run
+
+```
+$ php composer.phar require kartik-v/yii2-krajee-base "dev-master"
+```
+
+or add
+
+```
+"kartik-v/yii2-krajee-base": "dev-master"
+```
+
+to the ```require``` section of your `composer.json` file.
+
+### Widget
+Extends Yii [Widget](https://github.com/yiisoft/yii2/blob/master/framework/base/Widget.php) class for Krajee's Yii2 widgets and usage with bootstrap CSS framework. 
+
+### InputWidget
+Extends Yii [Yii InputWidget](https://github.com/yiisoft/yii2/blob/master/framework/widgets/InputWidget.php) class for Krajee's Yii2 widgets and usage with bootstrap CSS framework. 
+	
+### AssetBundle
+Extends Yii [AssetBundle](https://github.com/yiisoft/yii2/blob/master/framework/web/AssetBundle.php) class for Krajee's Yii2 widgets and usage with bootstrap CSS framework. 
+
+### Config
+A global configuration and validation helper class for usage across Krajee's Yii 2 extensions.
+
+## License
+
+**yii2-krajee-base** is released under the BSD 3-Clause License. See the bundled `LICENSE.md` for details.
