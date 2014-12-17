0505cdca6b792eb227208bfdcff9b705555aaf9d
Kartik Visweswaran <kartikv2@gmail.com>
2014-12-17 11:24:49 +0530

    Upgrade to release v1.6.0

--- PluginAssetBundle.php
+++ PluginAssetBundle.php
@@ -3,7 +3,7 @@
 /**
  * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2014
  * @package yii2-krajee-base
- * @version 1.6.0
+ * @version 1.5.0
  */
 
 namespace kartik\base;
@@ -16,9 +16,11 @@ namespace kartik\base;
  */
 class PluginAssetBundle extends AssetBundle
 {
+
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset'
+        'yii\bootstrap\BootstrapAsset',
         'yii\bootstrap\BootstrapPluginAsset'
     ];
+
 }
