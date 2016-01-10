0c274e31e4392cbafc5fe158eae81738794e02ce
Kartik Visweswaran <kartikv2@gmail.com>
2016-01-10 17:56:29 +0530

    Update to release v1.8.2

--- Html5InputAsset.php
+++ Html5InputAsset.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2014 - 2015
+ * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2014 - 2016
  * @package yii2-krajee-base
  * @version 1.8.2
  */
