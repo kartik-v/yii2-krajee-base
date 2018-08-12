555d1b0e95d676b3aeb35c9790b5f3afc63cd131
Kartik Visweswaran <kartikv2@gmail.com>
2018-08-12 09:08:35 +0530

    Update PHP Doc for `bsVersion`

--- src/InputWidget.php
+++ src/InputWidget.php
@@ -44,7 +44,18 @@ class InputWidget extends YiiInputWidget
     const LOAD_PROGRESS = '<div class="kv-plugin-loading">&nbsp;</div>';
 
     /**
-     * @var int|string the bootstrap library version
+     * @var int|string the bootstrap library version.
+     *
+     * To use with bootstrap 3 - you can set this to any string starting with 3 (e.g. `3` or `3.3.7` or `3.x`)
+     * To use with bootstrap 4 - you can set this to any string starting with 4 (e.g. `4` or `4.1.1` or `4.x`)
+     *
+     * This property can be set up globally in Yii application params in your Yii2 application config file.
+     *
+     * For example:
+     * `Yii::$app->params['bsVarsion'] = '4.x'` to use with Bootstrap 4.x globally
+     *
+     * If this property is set, this setting will override the `Yii::$app->params['bsVersion']`. If this is not set, and
+     * `Yii::$app->params['bsVersion']` is also not set, this will default to `3.x` (Bootstrap 3.x version).
      */
     public $bsVersion;
 
